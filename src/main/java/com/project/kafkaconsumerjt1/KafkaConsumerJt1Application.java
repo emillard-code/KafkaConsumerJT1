dc6e4bc695fbc06ea2f114baa3460b80b52e7ac0
Emerson Millard <emillard.code@gmail.com>
2022-08-19 01:30:59 -0700

    Initial commit

---
+++ src/main/java/com/project/kafkaconsumerjt1/KafkaConsumerJt1Application.java
@@ -0,0 +1,13 @@
+package com.project.kafkaconsumerjt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerJt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerJt1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/kafkaconsumerjt1/KafkaConsumerJt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kafkaconsumerjt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerJt1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
