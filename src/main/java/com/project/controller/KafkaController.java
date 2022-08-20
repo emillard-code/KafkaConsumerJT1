2ba20a1b3b6b9e89c1e5c0f952d5ba6e89cb3439
Emerson Millard <emillard.code@gmail.com>
2022-08-20 12:00:00 -0700

    Added controller methods.

--- src/main/java/com/project/controller/KafkaController.java
+++ src/main/java/com/project/controller/KafkaController.java
@@ -1,8 +1,30 @@
 package com.project.controller;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class KafkaController {
+
+    List<String> messages = new ArrayList<>();
+
+    @GetMapping("/consumeStringMessage")
+    public List<String> consumeMsg() {
+
+        return messages;
+        
+    }
+
+    @KafkaListener(groupId = "javatechie-1", topics = "javatechie", containerFactory = "kafkaListenerContainerFactory")
+    public List<String> getMsgFromTopic(String data) {
+
+        messages.add(data);
+        return messages;
+        
+    }
 
 }
